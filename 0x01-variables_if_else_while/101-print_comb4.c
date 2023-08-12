beda2c795e83bcca67e1ce8cbfe7e0256fbd891e
med099 <medouali099@gmail.com>
2023-08-11 19:57:06 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,11 +6,12 @@
  * Discription: prints all possible different combinations of three digits
  *
  * Return: Always 0 (Success)
- * 
+ *
  */
 int main(void)
 {
-	int n, m, l;
+	int n, m, l;i
+
 	for (n = 48; n < 58; n++)
 	{
 	for (m = 49; m < 58; m++)
