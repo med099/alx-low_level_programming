f87d804299e469f10621fb71f610ddaa14f6df86
med099 <medouali099@gmail.com>
2023-08-11 17:57:03 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,32 +2,33 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Function give type of number N P Z
+ * Description: Function to give type number
+ * positive, negative, zero.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
-	else
-	{
-	printf("%d is negative\n", n);
-	}
-	}
-	return (0);
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  if (n > 0)
+    {
+      printf("%d is positive\n", n);
+    }
+  else if (n == 0)
+    {
+      printf("%d is zero\n", n);
+    }
+  else
+    {
+      printf("%d is negative\n", n);
+    }
+
+  return (0);
 }
