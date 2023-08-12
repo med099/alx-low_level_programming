5d556bd2edee47936872f0276683c88af5d18747
med099 <medouali099@gmail.com>
2023-08-11 18:27:09 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet by usin putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+write(1,&c,1);
+c++;
+}
+write(1,"\n",1);
+
+return (0);
+}
