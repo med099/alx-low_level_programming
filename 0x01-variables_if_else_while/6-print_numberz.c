3588ca235635feb8c417c7eddccf61fd17923061
med099 <medouali099@gmail.com>
2023-08-11 19:01:47 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "0123456789", 9);
+
+return (0);
+}
