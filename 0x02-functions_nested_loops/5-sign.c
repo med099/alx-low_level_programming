cc2d09736a115c1bc2632342eff56fe638159197
med099 <medouali099@gmail.com>
2023-10-11 03:23:16 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - check lower case character
+ *@n: character to check it
+ * Return: 1 or 0.
+ */
+int	print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(-1);
+		return (-1);
+	}
+}
