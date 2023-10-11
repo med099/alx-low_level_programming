8d1617261528355fff7c3daaa47e3ef24f3da2e4
med099 <medouali099@gmail.com>
2023-10-11 04:04:13 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,14 @@
+#include <unistd.h>
 /**
- * print_last_digit - calculate the absolute of number.
- *@i: absolute.
- * Return: abs of number.
+ * print_last_digit - print last digit.
+ *@i: find last digit.
+ * Return: last digit.
  */
 int print_last_digit(int i)
 {
 	if (i < 0)
 		i = i * -1;
 	i = (i % 10) + 48;
-	write (1 , &i, 1);
+	write(1, &i, 1);
 	return (i);
 }
