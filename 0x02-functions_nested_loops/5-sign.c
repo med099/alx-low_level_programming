4ca062db1c1e7e9f4cb9370caf361922f17260de
med099 <medouali099@gmail.com>
2023-10-11 03:29:13 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 /**
  * print_sign - check lower case character
  *@n: character to check it
@@ -7,17 +8,17 @@ int	print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		write(1, '+', 1);
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar('0');
+		write(1, '0', 1);
 		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar('-');
+		write(1, '-', 1);
 		return (-1);
 	}
 }
