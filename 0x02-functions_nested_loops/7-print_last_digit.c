a792c4f774746148059123106ebf322aedb3e1ea
med099 <medouali099@gmail.com>
2023-10-11 04:14:00 +0100

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,11 @@
  */
 int print_last_digit(int i)
 {
+	int n;
+
 	if (i < 0)
 		i = i * -1;
-	i = (i % 10) + 48;
-	write(1, &i, 1);
-	return (i - 48);
+	n = (i % 10) + 48;
+	write(1, &n, 1);
+	return (i % 10);
 }
