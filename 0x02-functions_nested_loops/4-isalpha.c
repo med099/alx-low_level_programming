7d3e046bf473634cd7a68da397829e67cdf06fd8
med099 <medouali099@gmail.com>
2023-10-11 03:13:21 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
  *@c: character to check it
  * Return: 1 or 0.
  */
-int	_islalpha(int c)
+int	_isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) && (c >= 97 && >= z))
+	if ((c >= 65 && c <= 90) && (c >= 97 && c >= 'z'))
 		return (1);
 	return (0);
 }
