2fab98cde334f71ea6c96cd3d75113bfffcabe85
med099 <medouali099@gmail.com>
2023-10-11 03:30:14 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int	print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, '+', 1);
+		write(1, "+", 1);
 		return (1);
 	}
 	if (n == 0)
 	{
-		write(1, '0', 1);
+		write(1, "0", 1);
 		return (0);
 	}
 	if (n < 0)
 	{
-		write(1, '-', 1);
+		write(1, "-", 1);
 		return (-1);
 	}
 }
