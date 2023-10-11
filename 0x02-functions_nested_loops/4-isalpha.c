45e2923db588ea5a4b23910138ec909c790ecf59
med099 <medouali099@gmail.com>
2023-10-11 03:10:46 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - check lower case character
+ *@c: character to check it
+ * Return: 1 or 0.
+ */
+int	_islalpha(int c)
+{
+	if ((c >= 65 && c <= 90) && (c >= 97 && >= z))
+		return (1);
+	return (0);
+}
