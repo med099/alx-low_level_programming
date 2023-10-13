d106300e1181b39395a9c435bf607258190033f6
med099 <medouali099@gmail.com>
2023-10-13 03:33:51 +0100

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+nclude "holberton.h"
+/**
+ * print_square - draws square with #.
+ * @n: Side of square
+ * Return:  Void
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
