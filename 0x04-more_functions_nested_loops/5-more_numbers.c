b014834db19746bf218a881d1d224c5ecab9d9f8
med099 <medouali099@gmail.com>
2023-10-13 03:57:10 +0100

    taks 4 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints the range from 0 to 14  10times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			++j;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
