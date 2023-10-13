9491317bfa171a45d1719584a2ff652c79fa3f55
med099 <medouali099@gmail.com>
2023-10-13 03:50:44 +0100

    tastks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - checks uppercase
+ * @c: character to check
+ * Return: 1 if is uppercase else return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
