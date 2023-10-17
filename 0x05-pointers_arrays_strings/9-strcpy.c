300c3eb809732a65616df9d4dc184c931e6bc8b2
med099 <medouali099@gmail.com>
2023-10-17 03:00:16 +0100

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy - a function that copies the string src to dest
+ * @dest: destination
+ * @src: source
+ * return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
