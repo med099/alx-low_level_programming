29e09ee6e674affa6554b14bdd76e10153830810
med099 <medouali099@gmail.com>
2023-10-17 03:53:58 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,7 @@ int _atoi(char *s)
 	int i;
 	int sign;
 	int num;
+	int digit;
 
 	i = 0;
 	sign = 1;
@@ -21,7 +22,15 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		num = 10 * num + (s[i] - 48);
+		digit = s[i] - 48;
+		if (num > 2147483647 || (num == 214748364 && digit > 7))
+		{
+			if (sign == 1)
+				return (2147483647);
+			else
+				return (-2147483648);
+		}
+		num = 10 * num + digit;
 		i++;
 	}
 	return (sign * num);
