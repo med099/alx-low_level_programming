8c0f982c4111b9892663b3ce069b0e6c2fbe579f
med099 <medouali099@gmail.com>
2023-10-17 03:33:19 +0100

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+/**
+ * _atoi -  function that convert a string to an integer.
+ * @s: string
+ * Return: return integer
+ */
+
+void _atoi(char *s)
+{
+	int i;
+	int sign;
+	int num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = 10 * num + (s[i] - 48);
+		i++;
+	}
+	return (sign * num);
+}
