ec6e2518920a4200fc56187b3bf161c11f56ee92
med099 <medouali099@gmail.com>
2023-10-18 18:12:01 +0100

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,21 @@
-
+/**
+ * _strncpy - function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while(i < n && src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    if (!src[i])
-        dest[i] = '\0';
-    return (dest);
-}
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (!src[i])
+		dest[i] = '\0';
+	return (dest);
+	}
