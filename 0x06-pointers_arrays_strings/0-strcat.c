793b32cf3be19d6f8722cf2223e8ef844c969032
med099 <medouali099@gmail.com>
2023-10-18 17:10:32 +0100

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[j])
+		j++;
+	while (src[i])
+		src[i++] = dest[j++];
+	dest[j] = '\0';
+	return (dest);
+}
