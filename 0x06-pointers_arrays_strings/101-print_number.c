cfdd65a6e614ee2075751917b30a015a4d851dc1
med099 <medouali099@gmail.com>
2023-10-19 01:09:44 +0100

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number - function that print number
+ * @n: intger
+*/
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else if (n > 9)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	else
+		_putchar(n + 48);
+}
