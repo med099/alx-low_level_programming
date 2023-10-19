86b81fed4c5d54f54eceb35966c21c1e24ee8810
med099 <medouali099@gmail.com>
2023-10-20 00:25:16 +0100

    104104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,6 +19,8 @@ void print_buffer(char *b, int size)
 {
 	int i, j;
 
+	if (size > 0)
+	{
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
@@ -42,4 +44,8 @@ void print_buffer(char *b, int size)
 		}
 		printf("\n");
 	}
+	}
+	else
+		printf("\n");
 }
+
