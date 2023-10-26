bfeae31c9f0b0c0ceafc4bc0ba37ebf24a86a9ad
med099 <medouali099@gmail.com>
2023-10-26 02:27:04 +0100

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: string to count her length
+ * Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+	if (!(*s))
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);;
+}
