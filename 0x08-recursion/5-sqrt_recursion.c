5921aba4c1f54825a1437add39838d579aa5723e
med099 <medouali099@gmail.com>
2023-10-26 03:40:30 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,25 @@
 /**
- * _pow_recursion - function that returns the value of
- *  x raised to the power of y.
- * @x: number
- * @y: power
- * Return: x power y
+ * _sq_power_two - function that check number 'from 1 to Xnumber' power 2
+ *  .
+ * @sq: Xnumber
+ * @n: number to find its sqrt
+ * Return: sqrt of number
  */
-int _pow_recursion(int x, int y)
+int _sq_power_two(int sq, int n)
 {
-	if (y < 0)
+	if (sq * sq > n)
 		return (-1);
-	else if (y == 0)
-		return (1);
-	else if (y == 1)
-		return (x);
-	return (x * _pow_recursion(x, y - 1));
+	else if  (sq * sq == n)
+		return (sq);
+	return (_sq_power_two(sq + 1, n));
 }
 
 /**
  * _sqrt_recursion - function that returns the natural square root of a number.
  * @n: number
- * Return:  square root of a number.
+ * Return: square root of a number.
  */
 int _sqrt_recursion(int n)
 {
-	int	sqr;
-
-	sqr = 1;
-	if (n > 0)
-	{
-		while (sqr <= 46360)
-		{
-			if (_pow_recursion(sqr, 2) == n)
-				return (sqr);
-			sqr++;
-		}
-	}
-	return (-1);
+	return (_sq_power_two(1, n));
 }
