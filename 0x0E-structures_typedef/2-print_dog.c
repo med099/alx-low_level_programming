734257e0e241a88437eb2d68c95ca35d0258126f
med099 <medouali099@gmail.com>
2023-11-07 01:55:06 +0100

    taks 0 1 2 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: pointer to struct dog
+*/
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
