7671011e1eef8f499f448fb4d6bd952061ad72df
med099 <medouali099@gmail.com>
2023-11-09 00:25:35 +0100

    0-print_name.c 1-array_iterator.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+
+/**
+ * print_name - function that prints a name
+ * @name: name
+ * @f: function;
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function given as
+ * a parameter on each element of an array
+ * @array:  arry of int
+ * @size: size of arry
+ * @action: function pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
