2051286aea90b8bd5c722389cf1ce415c3f5c622
med099 <medouali099@gmail.com>
2023-11-09 02:59:09 +0100

    task3 program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,31 +14,26 @@ int main(int argc, char *argv[])
 	int (*opfunc)(int, int);
 	int n1, n2, res;
 
-	if (argc != 4)
+	if (argc == 4)
 	{
-		printf("Error\n");
-		return (-1);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[3]);
+		opfunc = get_op_func(argv[2]);
+		if (!opfunc)
+		{
+			printf("Error\n");
+			return (-1);
+		}
+		if ((*argv[2] == '/' || *argv[2] == '%') && n2 == 0)
+		{
+			printf("Error\n");
+			return (-1);
+		}
+		res = opfunc(n1, n2);
+		printf("%d\n", res);
+
+		return (0);
 	}
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	opfunc = get_op_func(argv[2]);
-
-	if (!opfunc)
-	{
-		printf("Error\n");
-		return (-1);
-	}
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && n2 == 0)
-	{
-		printf("Error\n");
-		return (-1);
-	}
-
-	res = func(n1, n2);
-
-	printf("%d\n", res);
-
-	return (0);
+	printf("Error\n");
+	return (-1);
 }
