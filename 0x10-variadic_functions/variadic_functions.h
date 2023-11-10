72ae55196653752ba4c6aa085e36006b0b456d32
med099 <medouali099@gmail.com>
2023-11-10 03:50:34 +0100

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
 
+/**
+ * struct Form - type format
+ * @type: type
+ * @f: pointer to function
+ */
+struct Form
+{
+	char *type;
+	void (*f)();
+};
+typedef struct Form _Form;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
