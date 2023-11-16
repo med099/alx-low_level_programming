32481c01bb72d1688d06865a0848790ae9395c90
med099 <medouali099@gmail.com>
2023-11-16 23:41:59 +0100

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number
+ * of elements in a linked list_t list
+ * @h: first element at list
+ * Return:  length of list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
