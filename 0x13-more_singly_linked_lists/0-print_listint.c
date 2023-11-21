2b9de7328f151cc974621afa4f9072538eae29b8
med099 <medouali099@gmail.com>
2023-11-21 16:44:20 +0100

    linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that print elements in a linked listint_t list
+ * @h: head of the list
+ * Return: number of elment
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * add_nodeint_end -  function that adds a new node
+ *  at the end of a listint_t list
+ * @head: head of the list
+ * @n: number to added at list
+ * Return: adress of elment or NULL if fails
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *current;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+}
